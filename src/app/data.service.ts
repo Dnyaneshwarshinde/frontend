5236113886d6a5924e5b1aee7d76d84661fb16a4
Dnyaneshwar <dnyaneshwarshinde003@gmail.com>
2019-11-18 22:10:18 +0530

    error solved

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -10,30 +10,30 @@ export class DataService {
 
   GetData()
   {
-   return this.http.get("http://200.0.1.156:4000/employees");
+   return this.http.get("http://192.168.43.73:4000/employees");
   }
 
   DeleteData(No)
   {
-    return this.http.delete("http://200.0.1.156:4000/employees/" +No );
+    return this.http.delete("http://192.168.43.73:4000/employees/" +No );
   }
 
   UpdateData(empObject)
   {
     console.log(empObject)
 
-    return this.http.put("http://200.0.1.156:4000/employees/" + empObject.No,empObject );
+    return this.http.put("http://192.168.43.73:4000/employees/" + empObject.No,empObject );
   }
 
   GetDataByID(No)
   {
     
-   return this.http.get("http://200.0.1.156:4000/employees/"+No );
+   return this.http.get("http://192.168.43.73:4000/employees/"+No );
   }
 
   AddData(empObject)
   {
-   return this.http.post("http://200.0.1.156:4000/employees", empObject);
+   return this.http.post("http://192.168.43.73:4000/employees", empObject);
   }
 }
 
